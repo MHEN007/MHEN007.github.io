6a29c2a123988795e6554c5079df0811c784be20
Matthew Mahendra <91794056+MHEN007@users.noreply.github.com>
2025-09-06 00:20:25 +0700

    Fix image file extension from .PNG to .png

--- app/components/home/AboutCard.tsx
+++ app/components/home/AboutCard.tsx
@@ -3,7 +3,7 @@ export default function AboutCard() {
   return(
     <div id="about-2" className="flex flex-col gap-3 items-center w-full md:w-[30%]">
       <div>
-        <Image src="/assets/Mahen.PNG" alt="Profile Picture" width={256} height={256} className="rounded-full w-32 h-32 md:w-64 md:h-64 border-4 border-gray-400"/>
+        <Image src="/assets/Mahen.png" alt="Profile Picture" width={256} height={256} className="rounded-full w-32 h-32 md:w-64 md:h-64 border-4 border-gray-400"/>
       </div>
       <div className="flex flex-col gap-3">
         <div className="text-md md:text-xl text-center md:text-left"> 
